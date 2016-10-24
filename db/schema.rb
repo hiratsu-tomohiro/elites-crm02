d60f13555ba7ecd325bb51d9e3aebc678c7d8036
HIRATSU TOMOHIRO <hiratsu-tomohiro@av.avex.co.jp>
2016-10-24 05:10:42 +0000

    06_実習終了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023064455) do
+ActiveRecord::Schema.define(version: 20161024045754) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20161023064455) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
